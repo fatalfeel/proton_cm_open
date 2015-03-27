7a16070e31d5de4a5c7fa03922a67b108104a0f5
fatalfeel <fatalfeel@hotmail.com>
2015-03-27 13:37:14 +0800

    black sprite fixed

--- shared/cocos2dx/sprite_nodes/CCSprite.cpp
+++ shared/cocos2dx/sprite_nodes/CCSprite.cpp
@@ -582,6 +582,12 @@ void CCSprite::draw(void)
         ccGLBindTexture2D(0);
     }
     
+    //black sprite bug fixed, by stone
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    
     //
     // Attributes
     //
