e6c2cfa10919aeed4c982701033366d7f0ba4f39
fatalfeel <fatalfeel@hotmail.com>
2015-06-25 22:54:27 +0800

    add options for android config

--- shared/Irrlicht/include/IrrCompileConfig.h
+++ shared/Irrlicht/include/IrrCompileConfig.h
@@ -107,6 +107,8 @@
 	#define _IRR_IPHONE_PLATFORM_	//share with android setting
 	#define _IRR_COMPILE_WITH_IPHONE_DEVICE_
 	
+	//one of 2 options can choice
+	//#define _IRR_COMPILE_WITH_OGLES1_
 	#define _IRR_COMPILE_WITH_OGLES2_
 #endif
 
