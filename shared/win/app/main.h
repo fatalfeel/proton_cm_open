d708d903babfb3aa05f3483255953b16c66d0f58
fatalfeel <fatalfeel@hotmail.com>
2015-08-25 01:27:19 +0800

    win touch change

--- shared/win/app/main.h
+++ shared/win/app/main.h
@@ -1,25 +1,46 @@
 //This file is used for all Windows builds and contains the WinMain entry point and Windows app code
-
 #pragma once
 
 //extern const char*	GetAppName();
 //extern const TCHAR*	GetAppNameW();
-extern bool	g_bAppFinished;
+extern bool			g_bAppFinished;
+extern int			g_winVideoScreenX;
+extern int			g_winVideoScreenY;
+extern bool			g_bIsFullScreen;
+extern std::string	g_videoModeNameForce; //if set, video mode will be forced to this instead of what is set in main.cpp
+
 void InitVideoSize();
-extern int g_winVideoScreenX;
-extern int g_winVideoScreenY;
-extern string g_videoModeNameForce; //if set, video mode will be forced to this instead of what is set in main.cpp
-extern bool g_bIsFullScreen;
 
 class VideoModeEntry
 {
 public:
 	VideoModeEntry(string _name, int _x, int _y, ePlatformID _platformID, eOrientationMode _forceOrientation = ORIENTATION_DONT_CARE) : name(_name), x(_x), y(_y), platformID (_platformID), forceOrientation (_forceOrientation){};
-	string name;
-	int x, y;
-	ePlatformID platformID;
-	eOrientationMode forceOrientation; //so LockLandscape() won't actually do it when using the 'iPhone Landscape' mode.. we 
+	
+	std::string			name;
+	int					x, y;
+	ePlatformID			platformID;
+	eOrientationMode	forceOrientation; //so LockLandscape() won't actually do it when using the 'iPhone Landscape' mode.. we 
 	//really want it to stay in portrait mode.
 };
 
+enum eWinActions
+{
+	ACTION_DOWN,
+	ACTION_UP,
+	ACTION_MOVE,
+	ACTION_CANCEL,
+	ACTION_OUTSIDE,
+};
 
+class WinMessageCache
+{
+public:
+	WinMessageCache()
+	{}
+	~WinMessageCache()
+	{}
+
+	eWinActions	type;
+	float		x,y;
+	int			finger;
+};
